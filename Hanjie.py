4c32e31c865a7c435adac232c777a6b895f62992
Morgus <allu2525@gmail.com>
2010-05-08 12:28:42 +0300

    Added GNU GPL

--- Hanjie.py
+++ Hanjie.py
@@ -1,3 +1,19 @@
+##    Hanjie - A nonogram puzzle game made with python
+##    Copyright (C) 2010 - Aleksi Blinnikka
+##
+##    This program is free software; you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation; either version 2 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License along
+##    with this program; if not, see <http://www.gnu.org/licenses/>.
+
 import menu, pygame
 from sys import exit as sysexit
 from os import environ
